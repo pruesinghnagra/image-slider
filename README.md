13fd141c76ccce14198cc0afdd06a77c2ac1cb5a
pruesinghnagra <pruesinghnagra@users.noreply.github.com>
2020-12-01 11:30:39 +1300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+### Boiling a plate for me.
+
+- Going to contain some cool shit that will freak you the fuck out.
---
+++ client/components/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <h1>Boil</h1>
+  )
+}
+
+export default App
